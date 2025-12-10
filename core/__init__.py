542992493373f21196f9f599f029e450881d428e
adornohenrique <henriquenobreadorno@hotmail.com>
2025-12-10 13:51:26 +0000

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -3,5 +3,4 @@ from .optimization import (
     baselines_mw,
     run_site_bess_mw,
     optimize_site_bess_mw,
-)# Aqui futuramente você pode pôr dataclasses/Pydantic models
-# por enquanto deixa vazio ou com algum comentário.
+)
